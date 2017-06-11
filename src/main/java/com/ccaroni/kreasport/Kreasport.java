f5e71e5d2aa7cc60883b68903a140b76f27bec64
Christopher Caroni <christopher.caroni@etudiant.univ-lille1.fr>
2017-06-11 23:34:50 +0100

    cherry picked base server with auth0 callback

---
+++ src/main/java/com/ccaroni/kreasport/Kreasport.java
@@ -0,0 +1,25 @@
+package com.ccaroni.kreasport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+/**
+ * Created by Master on 11/06/2017.
+ */
+@SpringBootApplication
+@ComponentScan(basePackages = {"com.ccaroni.kreasport", "com.auth0.spring.security.mvc"})
+@EnableAutoConfiguration
+@PropertySources({
+        @PropertySource("classpath:application.properties"),
+        @PropertySource("classpath:auth0.properties")
+})
+public class Kreasport {
+
+    public static void main(final String[] args) throws Exception {
+        SpringApplication.run(Kreasport.class, args);
+    }
+}
